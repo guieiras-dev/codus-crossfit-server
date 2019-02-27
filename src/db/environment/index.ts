898e47a5657d923ebfc5ea508c1b1793e58668b7
Guilherme Eiras <guialbertoeiras@hotmail.com>
2019-02-26 21:19:28 -0300

    Added database environments on json
    Co-authored-by: Emiliano Leite <leiteemiliano@gmail.com>

---
+++ src/db/environment/index.ts
@@ -0,0 +1,13 @@
+import { ConnectionOptions, getConnectionOptions } from "typeorm";
+import development from "./development.json";
+import test from "./test.json";
+
+export function getOptionsFromEnvironment(): Promise <ConnectionOptions> {
+  switch(process.env.NODE_ENV) {
+    case 'production': return getConnectionOptions();
+    case 'test': return Promise.resolve(test as ConnectionOptions);
+    default: return Promise.resolve(development as ConnectionOptions);
+  }
+};
+
+export default ({ development, test });
