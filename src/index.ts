c3c34e0627121bb073f8dd0ba962d1f0e3b3c1b2
Emiliano Leite <emiliano.leite@codus.com.br>
2019-03-27 09:48:18 -0300

    Add secretKey to GraphQL context
    This key will be used for the JWT generation
    
    Co-authored-by: Guilherme Eiras <guialbertoeiras@hotmail.com>

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,21 @@ import "reflect-metadata";
 import { connect } from "./db";
 import schema from "./graphql";
 
-const server = new GraphQLServer({ schema });
+if (process.env.NODE_ENV !== "production") {
+  // tslint:disable-next-line: no-var-requires
+  require("dotenv").config();
+}
+
+if (!process.env.SECRET_KEY) {
+  throw new Error("ENV VAR SECREY_KEY must be set!");
+}
+
+const server = new GraphQLServer({
+  schema,
+  context: () => {
+    return { secretKey: process.env.SECRET_KEY! };
+  },
+});
 
 connect().then(() => {
   server.start(() => {
