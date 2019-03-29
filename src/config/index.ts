60895ff87d88fbef7a668784ebb601a07cc88905
Guilherme Eiras <guialbertoeiras@hotmail.com>
2019-03-28 23:55:49 -0300

    Add passport jwt middlewares on server config
    Also, set current user on GraphQL context
    
    Co-authored-by: Emiliano Leite <leiteemiliano@gmail.com>

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,15 +1,45 @@
+import { NextFunction, Request, Response } from "express";
 import { GraphQLServer } from "graphql-yoga";
+import Passport from "passport";
+import { ExtractJwt, Strategy as JWTStrategy } from "passport-jwt";
+
+import User from "../entities/user";
 import schema from "../graphql";
 
 export default function configServer() {
   ensureEnvVars();
 
-  return new GraphQLServer({
+  Passport.use(new JWTStrategy({
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.SECRET_KEY!,
+  }, async (jwtPayload, done) => {
+    try {
+      const user = await User.findOneOrFail({ id: jwtPayload.id });
+      return done(null, user);
+    } catch (error) {
+      return done(error, false);
+    }
+  }));
+
+  const server = new GraphQLServer({
     schema,
-    context: () => {
-      return { secretKey: process.env.SECRET_KEY! };
+    context: ({ request }) => {
+      return {
+        secretKey: process.env.SECRET_KEY!,
+        user: request.user,
+      };
     },
   });
+  server.use((req: Request, res: Response, next: NextFunction) => {
+    if (req.headers.authorization) {
+      return Passport.authenticate("jwt", { session: false })(req, res, next);
+    } else {
+      req.user = null;
+      return next();
+    }
+  });
+
+  return server;
 }
 
 function ensureEnvVars() {
